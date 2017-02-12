5b82aa2f677a39ebf0d95d020e45bcffc8f6f271
t-kuester <tobias.kuester@gmx.net>
2017-02-12 14:03:38 +0100

    fractal mountain with shared lines, but not quite right yet

--- automata/fractal_mountain.py
+++ automata/fractal_mountain.py
@@ -2,43 +2,67 @@
 
 """Fractal Mountain Generator
 by Tobias Kuester, <tobias.kuester@gmx.net>, 2017
-
 """
 
 import random, cmath, math
 import tkinter
 
+# factor for randomization as fraction of triangle side length
+FACTOR = 0.0
 
-def randomize(point, magnitude):
-	return complex(random.gauss(point.real, magnitude),
-	               random.gauss(point.imag, magnitude))
+class Line(object):
+	
+	def __init__(self, s, t, left=None, right=None):
+		self.s = s
+		self.t = t
+		self.left = left
+		self.right = right
+	
+	def __repr__(self):
+		return "Line(%r, %r, %r, %r)" % (self.s, self.t, self.left, self.right)
+		
+	def split(self):
+		if self.left is None and self.right is None:
+			length = abs(self.s - self.t)
+			midpoint = (self.s + self.t) / 2
+			p = complex(random.gauss(midpoint.real, length * FACTOR),
+	                    random.gauss(midpoint.imag, length * FACTOR))
+			self.left = Line(self.s, p)
+			self.right = Line(p, self.t)
+		return self.left, self.right
 
 
-class FractalMountain(object):
+class FractalTriangle(object):
 
-	def __init__(self, f, a, b, c):
-		self.points = a, b, c
-		self.factor = f
-		self.children = []
+	def __init__(self, ab, bc, ca, children=None):
+		self.sides = ab, bc, ca
+		self.children = children
+		
+	def __repr__(self):
+		s1, s2, s3 = self.sides
+		return "FractalTriangle(%r, %r, %r, %r)" % (s1, s2, s3, self.children)
 		
 	def expand(self):
 		if self.children:
 			for child in self.children:
 				child.expand()
 		else:
-			a, b, c = self.points
-			ab = (a + b) / 2
-			bc = (b + c) / 2
-			ca = (c + a) / 2
-			factor = self.factor * (abs(a-b) + abs(b-c) + abs(c-a)) / 3
-			ab, bc, ca = (randomize(x, factor) for x in (ab, bc, ca))
+			ab, bc, ca = self.sides
+			ab1, ab2 = ab.split()
+			bc1, bc2 = bc.split()
+			ca1, ca2 = ca.split()
 			
-			for points in ((a, ab, ca), (b, bc, ab), (c, bc, ca), (ab, bc, ca)):
-				self.children.append(FractalMountain(self.factor, *points))
-				
+			a = Line(ab1.t, ca2.s)
+			b = Line(bc1.t, ab2.s)
+			c = Line(ca1.t, bc2.s)
+			
+			triplets = ((a, ca2, ab1), (b, ab2, bc1), (c, bc2, ca1), (a, b, c))
+			#~ triplets = ((a, ca2, ab1), (b, ab2, bc1), (c, bc2, ca1))
+			self.children = [FractalTriangle(*sides) for sides in triplets]
+			
 	def get_leafs(self):
 		if self.children:
-			return [leaf for child in self.children for leaf in child.get_leafs()]
+			return (leaf for child in self.children for leaf in child.get_leafs())
 		return [self]
 
 
@@ -67,7 +91,8 @@ class FractalMountainFrame(tkinter.Frame):
 		a = complex(WIDTH * .25, HEIGHT * .75)
 		b = complex(WIDTH * .50, HEIGHT * .25)
 		c = complex(WIDTH * .75, HEIGHT * .75)
-		self.mountain = FractalMountain(0.05, a, b, c)
+		self.mountain = FractalTriangle(Line(a, b), Line(b, c), Line(c, a))
+		print(self.mountain)
 		self.paint_mountain()
 		    
 	def expand(self):
@@ -79,9 +104,11 @@ class FractalMountainFrame(tkinter.Frame):
 		self.canvas.delete("all")
 		if self.mountain:
 			for leaf in self.mountain.get_leafs():
-				a, b, c = leaf.points
-				points = [p for x in (a, b, c, a) for p in (x.real, x.imag)]
-				self.canvas.create_line(points)
+				#~ a, b, c = (s.s for s in leaf.sides)
+				#~ points = [p for x in (a, b, c, a) for p in (x.real, x.imag)]
+				#~ self.canvas.create_line(points)
+				for side in leaf.sides:
+					self.canvas.create_line(side.s.real, side.s.imag, side.t.real, side.t.imag)
 				
 
 # create and start FracTreeFrame
