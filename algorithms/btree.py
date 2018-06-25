e9216e1a69dc07caca4ed6205fb95cf628708cbd
t-kuester <tobias.kuester@gmx.net>
2018-06-25 18:41:54 +0200

    tree balancing

--- algorithms/btree.py
+++ algorithms/btree.py
@@ -3,7 +3,7 @@
 
 class BTree:
 	"""Class representing a Binary Search Tree.
-	TODO: delete, balance, rotateleft, rotateright, popmin, popmax
+	TODO: delete arbitrary nodes
 	"""
 
 	def __init__(self, value=None, left=None, right=None, count=0, d=0):
@@ -17,21 +17,67 @@ class BTree:
 		self.count = count
 		self.d = d
 
-	def insert(self, value):
+	def insert(self, value, count=1):
 		"""Insert element into tree. Either add a new node in the correct place
 		in the tree, or increment the count for an existing element.
 		"""
 		if self.value is None:
 			self.value = value
-			self.count = 1
+			self.count = count
 			self.left = BTree(d=self.d + 1)
 			self.right = BTree(d=self.d + 1)
 		elif value == self.value:
-			self.count += 1
+			self.count += count
 		elif value < self.value:
-			self.left.insert(value)
+			self.left.insert(value, count)
 		else:
-			self.right.insert(value)
+			self.right.insert(value, count)
+
+	def balance(self):
+		"""Balance the tree by rotating the subtrees, i.e. removing the max from
+		the left tree, using it as the value, and inserting the old value into
+		the right tree, or vice versa.
+		"""
+		if self.value is not None:
+			while True:
+				# balance subtrees
+				self.left.balance()
+				self.right.balance()
+				lh, rh = self.left.height(), self.right.height()
+				if lh > rh + 1:
+					# rotate right
+					self.right.insert(self.value, self.count)
+					self.value, self.count = self.left.popmax()
+				elif lh + 1 < rh:
+					# rotate left
+					self.left.insert(self.value, self.count)
+					self.value, self.count = self.right.popmin()
+				else:
+					break
+
+	def popmin(self):
+		"""Remove minimum elements from tree and return value and count.
+		"""
+		if self.value is None:
+			return None, 0
+		if self.left.value is not None:
+			return self.left.popmin()
+		else:
+			v, c = self.value, self.count
+			self.value, self.count = self.right.popmin()
+			return v, c
+
+	def popmax(self):
+		"""Remove maximum elements from tree and return value and count.
+		"""
+		if self.value is None:
+			return None, 0
+		if self.right.value is not None:
+			return self.right.popmax()
+		else:
+			v, c = self.value, self.count
+			self.value, self.count = self.left.popmax()
+			return v, c
 
 	def contains(self, value):
 		"""Check whether the given element is contained in the tree and return
@@ -97,20 +143,23 @@ class BTree:
 
 	def height(self):
 		"""Get the height of the tree."""
-		return self.d if self.value is None else max(self.left.height(), self.right.height())
+		return 0 if self.value is None else 1 + max(self.left.height(),
+		                                            self.right.height())
 
 	def __str__(self):
 		s = "  " * self.d
 		if self.value is None:
 			return s + "-"
 		else:
-			return "%s(%r *%d\n%s\n%s)" % (s, self.value, self.count, self.left, self.right)
+			return "%s(%r *%d\n%s\n%s)" % (s, self.value, self.count,
+					                       self.left, self.right)
 
 	def __repr__(self):
 		return "BTree(%r, %r, %r)" % (self.value, self.left, self.right)
 
 
 def from_list(lst):
+	"""Create BTree from list."""
 	tree = BTree()
 	for x in lst:
 		tree.insert(x)
@@ -120,31 +169,34 @@ def from_list(lst):
 # TESTING
 if __name__ == "__main__":
 	import random
-	N, M = 100, 20
+	N, M = 100, 100
 
 	# create random numbers, put numbers into tree
 	values = [random.randint(0, M) for _ in range(N)]
 	tree = from_list(values)
-	print(tree)
-	print(tree.height())
-	print(tree.num())
+	assert tree.num() == len(values)
 
 	# min and max elements
-	print(tree.getmin(), tree.getmax())
-	print(tree.getmin() == min(values))
-	print(tree.getmax() == max(values))
+	assert tree.getmin() == min(values)
+	assert tree.getmax() == max(values)
 
 	# check whether inorder is same as ordered list
-	print(sorted(values) == tree.inorder())
+	assert sorted(values) == tree.inorder()
 
 	# check for all numbers whether they are in the tree
 	pos = set(values)
 	neg = [x for x in range(M) if x not in pos]
-	print(all(tree.contains(x) for x in pos))
-	print(all(not tree.contains(x) for x in neg))
+	assert all(tree.contains(x) for x in pos)
+	assert all(not tree.contains(x) for x in neg)
 
 	# lower and higher partitions
 	low = tree.lower(M//2)
 	high = tree.upper(M//2)
-	print(low.inorder() == sorted(x for x in values if x < M//2))
-	print(high.inorder() == sorted(x for x in values if x > M//2))
+	assert low.inorder() == sorted(x for x in values if x < M//2)
+	assert high.inorder() == sorted(x for x in values if x > M//2)
+
+	# balancing
+	tree.balance()
+	assert sorted(values) == tree.inorder()
+	print(tree)
+	print("height", tree.height())
